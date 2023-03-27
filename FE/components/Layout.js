73300894e280178e7febdbfe05d342988eb3e935
ericagong <48196721+ericagong@users.noreply.github.com>
2023-03-27 15:53:34 +0900

    feat: 반응형 디자인

--- FE/components/Layout.js
+++ FE/components/Layout.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const Layout = ({ children }) => {
+  const { Search } = Input;
+
+  const onSearch = (value) => console.log(value);
+
   const items = [
     {
       label: <Link href="/">홈</Link>,
@@ -11,6 +15,17 @@ const Layout = ({ children }) => {
     {
       label: <Link href="/profile">프로필</Link>,
       key: "profile",
+    },
+    {
+      label: (
+        <Search
+          placeholder="검색어를 입력하세요."
+          onSearch={onSearch}
+          enterButton
+          style={{ verticalAlign: "middle" }}
+        />
+      ),
+      key: "searchBar",
     },
     {
       label: <Link href="/signUp">회원가입</Link>,
@@ -22,8 +37,24 @@ const Layout = ({ children }) => {
     <div>
       <div>
         <Menu items={items} mode="horizontal" />
+        <Row gutter={8}>
+          <Col xs={24} md={6}>
+            왼쪽 메뉴
+          </Col>
+          <Col xs={24} md={12}>
+            {children}
+          </Col>
+          <Col xs={24} md={6}>
+            <a
+              href="https://github.com/ericagong"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Made by Pantheon
+            </a>
+          </Col>
+        </Row>
       </div>
-      {children}
     </div>
   );
 };
