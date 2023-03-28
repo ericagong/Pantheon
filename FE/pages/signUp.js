aaf5bdbb71942f11e02881ab920df72337584c52
ericagong <48196721+ericagong@users.noreply.github.com>
2023-03-28 13:57:54 +0900

    feat: useInput 커스텀훅 생성

--- FE/pages/signUp.js
+++ FE/pages/signUp.js
@@ -1,13 +1,21 @@
+import { useCallback } from "react";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import { Form } from "antd";
 
 const SignUp = () => {
+  const onSignUp = useCallback(() => {
+    // antd에서는 e.preventDefault() 가 이미 적용되어 있음
+  }, []);
+
   return (
     <>
       <Head>
         <title>회원가입 | Pantheon</title>
       </Head>
-      <Layout>SignUp Page</Layout>
+      <Layout>
+        <Form onFinish={onSignUp}></Form>
+      </Layout>
     </>
   );
 };
