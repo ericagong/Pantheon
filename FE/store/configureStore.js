0d661c403bb53601ae9cb4667dea72a536198832
ericagong <48196721+ericagong@users.noreply.github.com>
2023-03-29 13:28:14 +0900

    feat: logger 및 thunk 연결

--- FE/store/configureStore.js
+++ FE/store/configureStore.js
@@ -1,10 +1,20 @@
 import { createWrapper } from "next-redux-wrapper";
 import { createStore, compose, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
 import reducer from "../reducers/index";
 
+// custom logger 미들웨어
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action);
+    return next(action);
+  };
+
 const configureStore = () => {
-  const middlewares = [];
+  const middlewares = [thunkMiddleware, loggerMiddleware];
 
   // 개발 시만 devTool 연결
   const enhancer =
